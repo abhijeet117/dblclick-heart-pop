520f01c45fe2ff8997b73a1e1792422d65c5092e
abhijeet117 <abhijeetkumarsah92@gmail.com>
2025-11-21 12:02:42 +0530

    adde function for licking img

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ let heart = document.querySelector('i');
 
 img.addEventListener('dblclick', function() {
     heart.style.opacity = '1';
-    console.log('hi')
+    heart.style.transform = 'translate(-50%, -50%) scale(1.6) rotate(0deg)';
+
+    
+
+    setTimeout(()=> {
+        heart.style.transform = 'translate(-50%, -500%) scale(1) rotate(45deg)';
+    }, 700)
+
+    setTimeout(()=> {
+        heart.style.opacity = '0';
+    }, 1200)
+
+    setTimeout(()=> {
+        heart.style.transform = 'translate(-50%, -50%) scale(0) rotate(-45deg)';
+    }, 1500)
+    
 
 })
