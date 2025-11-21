e3464fb0a54f63fa73e7508cc67521fd42cbdf35
abhijeet117 <abhijeetkumarsah92@gmail.com>
2025-11-21 11:13:36 +0530

    added some heart logic

---
+++ script.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('img');
+
+let heart = document.querySelector('i');
+
+img.addEventListener('dblclick', function() {
+    heart.style.opacity = '1';
+    console.log('hi')
+
+})
